1af70fdca96e7932637c078d36719655b56ec852
ashrafsalem2 <eng.ashrafsalem2@gmail.com>
2024-11-19 21:36:59 +0200

    front application Daniels website

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+//get collapse ul
+let mainDiv = document.querySelector('.collapse');
+
+/**
+ * to close collapse if x or outside is clicked
+ */
+function closePanel(){
+    mainDiv.classList.remove('show');
+}
